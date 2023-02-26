a8a12493bc22d62c111e3d4dc5a6411ced8758aa
€$¥ <dev@krk.finance>
2023-02-26 01:29:03 +0100

    added responsiveness for navbar links

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -10,7 +10,49 @@ export const NavBar: FC = () => {
       {/* NavBar / Header */}
       <div className="navbar fixed z-[999] flex h-[67.5px] flex-row border-white bg-black text-[21px] text-neutral-content shadow-lg md:pb-2">
         <div className="z-2 navbar-start flex">
-          <div className="flex content-center">
+          <div className="flex-none lg:hidden">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn-ghost btn-circle btn">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-6 w-6 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="dropdown-content rounded-box menu-compact mt-3 w-52 bg-black p-2 shadow"
+              >
+                <li className="list-none">
+                  <Link
+                    href="https://coinablepay.com/store/deans-list"
+                    target={"_blank"}
+                    rel="noreferrer"
+                  >
+                    <div className="btn-ghost rounded-btn btn-md btn">
+                      SERVICES
+                    </div>
+                  </Link>
+                </li>
+                <li className="list-none">
+                  <Link href="/joindl">
+                    <button className="btn-ghost rounded-btn btn-md btn">
+                      JOIN US
+                    </button>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex hidden content-center lg:flex">
             <Link href="/">
               <Image
                 src="/images/logo-dl.svg"
@@ -30,22 +72,16 @@ export const NavBar: FC = () => {
 
         {/* Wallet & Settings */}
         <div className="navbar-end">
-          <div className="flex-none lg:hidden">
-            <label htmlFor="my-drawer-3" className="btn-ghost btn-square btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-6 w-6 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            </label>
+          <div className="flex content-center lg:hidden">
+            <Link href="/">
+              <Image
+                src="/images/logo-dl.svg"
+                alt="DEAN'S LIST"
+                className="h-40 w-40"
+                height={160}
+                width={160}
+              />
+            </Link>
           </div>
           <Link
             href="https://coinablepay.com/store/deans-list"
